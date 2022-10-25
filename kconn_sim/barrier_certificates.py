11fcf29ead638ce67177191396bef078836cdc79
Jason <jhughes50@fordham.edu>
2022-10-24 23:29:48 -0400

    Updates to barrier certificates

--- kconn_sim/barrier_certificates.py
+++ kconn_sim/barrier_certificates.py
@@ -3,13 +3,49 @@ import numpy as np
 class BarrierCertificates:
 
     def __init__(self):
+        pass
 
-        self.radius_safety = 1
-        self.radius_connectivity = 50
+    def dist(self, x, y):
+        return np.linalg.norm(x - y)**2
 
+    def construct_A(self, swarm, n):
+        A = np.zeros((n,n))
+        for i in range(n-1):
+            for j in range(i,n):
+                A[i][i] = -2 * (swarm[i].location - swarm[j].location) * swarm[i].desired_control
+                A[i][j] = 2 * (swarm[i].location - swarm[j].location) * swarm[j].desired_control
+        return A
+        
+class ConnectionCertificate(BarrierCertificates):
 
-    def safety_check(self, x, y):
-        return np.linalg.norm(x - y) > self.radius_safety
+    def __init__(self, r, s, g=1):
 
-    def connection_check(self, x, y):
-        return np.linalg.norm(x - y) <= self.radius_connectivity
+        super().__init__()
+        self.radius = r
+        self.swarm = s
+        self.n = len(s)
+        self.A = np.zeros((self.n,self.n))
+        self.gamma = g
+
+    def update_A(self):
+        self.A = self.construct_A(self.swarm, self.n)
+
+    def get_b(self, i, j):
+        return self.gamm * self.dist(self.swarm[i].location, self.swarm[j].location) + self.radius**2)
+
+class SafetyCertificate(BarrierCertificates):
+
+    def __init__(self, r, s, g=1):
+
+        super().__init__()
+        self.radius = r
+        self.swarm = s
+        self.n = len(s)
+        self.A = np.zeros((self.n,self.n))
+        self.gamma = g
+        
+    def update_A(self):
+        self.A = self.construct_A(self.swarm, self.n)
+            
+    def get_b(self, i, j):
+        return self.gamma * (self.dist(self.swarm[i].location, self.swarm[j].location) - self.radius**2)
